4698e40c20dadbf9dc11e7590992d9796043155f
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-22 23:37:04 -0700

    Create Punto 6.py

---
+++ Punto 6.py
@@ -0,0 +1,9 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("---------------Punto 6---------------")
+e = input("ingresa una frase o oracion: ")#capturar valor
+def inv(ri):#crear funcion
+    print("--------------------------------")
+    print ((ri)[::-1])#la parte del -1 hase que en resumen dise que el valor ri se leea alrebes
+inv(e)#mostrar valor
+print("-------------------------------------")
+print("Resultado: la funcion sirve para invertir cadenas de texto.\n")
